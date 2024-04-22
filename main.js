86febdd983cf1169939fb55d87cd4599b556f59c
Santiago Trini <santiagotrini@gmail.com>
2024-04-22 17:44:46 -0300

    agrega hex al script

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function handleSubmit(event) {
   event.preventDefault();
   let n = +document.forms[0].n.value;
-  let h3 = document.querySelector('#bin');
-  h3.textContent = n.toString(2);
+  let bin = document.querySelector('#bin');
+  bin.textContent = n.toString(2);
+  let hex = document.querySelector('#hex');
+  hex.textContent = n.toString(16);
 }
